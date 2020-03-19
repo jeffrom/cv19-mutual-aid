8303d7fe4b6b29dcc47b3970d077ad7794e4d3f2
Jeff Martin <jeff@teacherspayteachers.com>
2020-03-19 15:44:56 -0700

    tweak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 export const Footer = () => <footer>
-  <a href="https://github.com/jeffrom/cv19-mutual-aid">Want to contribute?</a>
+  <p>
   <small>We list these networks as a public resource. We cannot verify or vouch for any network or individual offerings. Please exercise all necessary judgement when interacting with community members not previously known to you.</small>
+  <a href="https://github.com/jeffrom/cv19-mutual-aid">Want to contribute?</a>
+  </p>
   <style jsx>{`
     footer {
       width: 100%;
