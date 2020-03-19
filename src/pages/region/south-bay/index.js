dc2ff96afdc615b79e9e43c987a71d78a720785a
Jeff Martin <jeffmartin@gmail.com>
2020-03-19 00:30:38 -0700

    split up bay area regions

---
+++ src/pages/region/south-bay/index.js
@@ -0,0 +1,6 @@
+import { Region } from '../../../components/Region'
+import resourceLinks from './resource-links.yml'
+
+const SanFrancisco = () => <Region region="Bay Area" resourceLinks={resourceLinks} />
+
+export default SanFrancisco
